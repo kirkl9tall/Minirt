a399fcd54f89ba021a7690f5735ae49196a81e54
Abdelilah Ismaili Alaoui <abismail@c2r3p8.1337.ma>
2025-11-05 16:32:38 +0100

    cylinder infini

--- Render/shapes.c
+++ Render/shapes.c
@@ -32,7 +32,6 @@ int	hit_sphere(t_ray ray, t_sphere *sphere, t_hit *hit)
 	double	sqrt_d;
 	double	t1;
 	double	t2;
-
 	oc = vect_subs(ray.origin, sphere->sph_center);
 	a = vect_prod(ray.direction, ray.direction);
 	b = 2.0 * vect_prod(oc, ray.direction);
@@ -58,12 +57,53 @@ int	hit_sphere(t_ray ray, t_sphere *sphere, t_hit *hit)
 
 int	hit_cylinder(t_ray ray, t_cylin *cy, t_hit *hit)
 {
-	(void)ray;
-	(void)cy;
-	(void)hit;
+	t_vec3	oc;
+	double	a;
+	double	b;
+	double	c;
+	double	delta;
+	t_vec3 p;
+	t_vec3 z;
+	double x1;
+	double x2;
+	double x;
+	double sqrt_d;
+	
+	oc = vect_subs(ray.origin , cy->cy_center);
+	p = vect_subs(oc,vect_multi(cy->nv_cy,vect_prod(cy->nv_cy,oc)));
+	z = vect_subs(ray.direction, vect_multi(cy->nv_cy,vect_prod(cy->nv_cy,ray.direction)));
+	a = vect_prod(z,z);
+	b = 2 * (vect_prod(p,z));
+	c = vect_prod(p,p) - ((cy->cy_diam/2) * (cy->cy_diam/2));
+
+	delta = b * b - 4 * (a * c);
+	if (delta  < 0)
+		return  0;
+	sqrt_d = sqrt(delta);
+	x1 = (-b - sqrt_d) / (2.0 * a);
+	x2 = (-b + sqrt_d) / (2.0 * a);
+	if (x1 < 1e-6 && x2 < 1e-6)
+    	return 0;
+
+	if (x1 > 0 && x2 > 0)
+	x = fmin(x1,x2);
+	else if (x1 > 0)
+	x = x1;
+	else if (x2 > 0 )
+	x = x2;
+	if (x < hit->t)
+	{
+		hit->t = x;
+		hit->color = cy->color;
+		cy->nv_cy = vect_normalized(cy->nv_cy);
+		hit->point = vect_addi(ray.origin , vect_multi(ray.direction,x));
+		t_vec3 tmp = vect_subs(hit->point, cy->cy_center);
+		t_vec3 proj = vect_multi(cy->nv_cy, vect_prod(cy->nv_cy, tmp));
+		hit->normal = vect_normalized(vect_subs(tmp, proj));
+		return 1;
+	}
 	return (0);
 }
-
 
 int	find_closest_hit(t_mini *mini, t_ray ray, t_hit *hit)
 {
@@ -72,7 +112,8 @@ int	find_closest_hit(t_mini *mini, t_ray ray, t_hit *hit)
 	t_hit		temp_hit;
 	t_sphere	*current_sph;
 	t_plan		*current_plane;
-
+	t_cylin		*current_cylinder;
+	current_cylinder = mini->cy;
 	hit_anything = 0;
 	closest_so_far = INFINITY;
 	current_sph = mini->sph;
@@ -98,6 +139,19 @@ int	find_closest_hit(t_mini *mini, t_ray ray, t_hit *hit)
 			hit_anything = 1;
 		}
 		current_plane = current_plane->next;
+	}
+	while (current_cylinder)
+	{
+		t_hit temp_hit;
+		temp_hit.t = closest_so_far;
+		if (hit_cylinder(ray, current_cylinder, &temp_hit)
+		&& temp_hit.t < closest_so_far)
+		{
+			closest_so_far = temp_hit.t;
+			*hit = temp_hit;
+			hit_anything = 1;
+		}
+		current_cylinder = current_cylinder->next;
 	}
 	return (hit_anything);
 }
--- test.c
+++ test.c
@@ -1,79 +1,97 @@
 #include "minirt.h"
 
-int	main(void)
-{
-	t_mini	mini;
-	double	aspect_ratio;
-	double	scale;
+// int	main(void)
+// {
+// 	t_mini	mini;
+// 	double	aspect_ratio;
+// 	double	scale;
 
-	// Initialize MLX
-	mini.mlx_utils.mlx = mlx_init();
-	if (!mini.mlx_utils.mlx)
-		return (1);
-	mini.mlx_utils.win = mlx_new_window(mini.mlx_utils.mlx, W_W, W_H,
-			"Ray Tracer Test");
-	if (!mini.mlx_utils.win)
-		return (1);
-	mini.mlx_utils.img = mlx_new_image(mini.mlx_utils.mlx, W_W, W_H);
-	if (!mini.mlx_utils.img)
-		return (1);
-	mini.mlx_utils.addr = mlx_get_data_addr(mini.mlx_utils.img,
-			&mini.mlx_utils.bpp, &mini.mlx_utils.size_line,
-			&mini.mlx_utils.endian);
+// 	// Initialize MLX
+// 	mini.mlx_utils.mlx = mlx_init();
+// 	if (!mini.mlx_utils.mlx)
+// 		return (1);
+// 	mini.mlx_utils.win = mlx_new_window(mini.mlx_utils.mlx, W_W, W_H,
+// 			"Ray Tracer Test");
+// 	if (!mini.mlx_utils.win)
+// 		return (1);
+// 	mini.mlx_utils.img = mlx_new_image(mini.mlx_utils.mlx, W_W, W_H);
+// 	if (!mini.mlx_utils.img)
+// 		return (1);
+// 	mini.mlx_utils.addr = mlx_get_data_addr(mini.mlx_utils.img,
+// 			&mini.mlx_utils.bpp, &mini.mlx_utils.size_line,
+// 			&mini.mlx_utils.endian);
 
-	// Initialize camera with proper FOV
-	mini.cam.pos = (t_vec3){0, 0, 0};
-	mini.cam.orient = vect_normalized((t_vec3){0, 0, 1});
-	mini.cam.up_v = vect_normalized((t_vec3){0, 1, 0});
-	mini.cam.right_v = vect_normalized(vect_cross(mini.cam.orient, mini.cam.up_v));
+// 	// Initialize camera with proper FOV
+// 	mini.cam.pos = (t_vec3){0, 0, 0};
+// 	mini.cam.orient = vect_normalized((t_vec3){0, 0, 1});
+// 	mini.cam.up_v = vect_normalized((t_vec3){0, 1, 0});
+// 	mini.cam.right_v = vect_normalized(vect_cross(mini.cam.orient, mini.cam.up_v));
 	
-	// Use FOV to calculate viewport size
-	double fov = 60.0;
-	aspect_ratio = (double)W_W / (double)W_H;
-	scale = tan(fov * 0.5 * M_PI / 180.0);
-	mini.cam.width_canva = 2.0 * scale * aspect_ratio;
-	mini.cam.height_canva = 2.0 * scale;
+// 	// Use FOV to calculate viewport size
+// 	double fov = 60.0;
+// 	aspect_ratio = (double)W_W / (double)W_H;
+// 	scale = tan(fov * 0.5 * M_PI / 180.0);
+// 	mini.cam.width_canva = 2.0 * scale * aspect_ratio;
+// 	mini.cam.height_canva = 2.0 * scale;
 
-	// Create a sphere
-	mini.sph = malloc(sizeof(t_sphere));
-	if (!mini.sph)
-		return (1);
-	mini.sph->sph_center = (t_vec3){0, 0, 5};
-	mini.sph->s_diam = 2.0;
-	mini.sph->color = (t_vec3){255, 0, 0};
-	mini.sph->next = NULL;
+// 	// Create a sphere
+// 	mini.sph = malloc(sizeof(t_sphere));
+// 	if (!mini.sph)
+// 		return (1);
+// 	mini.sph->sph_center = (t_vec3){0, 0, 5};
+// 	mini.sph->s_diam = 2.0;
+// 	mini.sph->color = (t_vec3){255, 0, 0};
+// 	mini.sph->next = NULL;
 
-	// Create a plane  
-	mini.plane = malloc(sizeof(t_plan));
-	if (!mini.plane)
-		return (1);
-	mini.plane->cor_plan = (t_vec3){0, -2, 0};
-	mini.plane->nnv_plan = (t_vec3){0, 1, 0};
-	mini.plane->color = (t_vec3){0, 255, 0};
-	mini.plane->next = NULL;
+// 	// Create a plane  
+// 	mini.plane = malloc(sizeof(t_plan));
+// 	if (!mini.plane)
+// 		return (1);
+// 	mini.plane->cor_plan = (t_vec3){0, -2, 0};
+// 	mini.plane->nnv_plan = (t_vec3){0, 1, 0};
+// 	mini.plane->color = (t_vec3){0, 255, 0};
+// 	mini.plane->next = NULL;
 
-	// Set cylinder to NULL
-	mini.cy = NULL;
+// 	// Set cylinder to NULL
+// 	mini.cy = NULL;
 
-	// ADD AMBIENT LIGHTING (THIS WAS MISSING!)
-	mini.amb.alr = 0.3;  // 30% ambient light
-	mini.amb.color = (t_vec3){255, 255, 255};  // White ambient light
+// 	// ADD AMBIENT LIGHTING (THIS WAS MISSING!)
+// 	mini.amb.alr = 0.3;  // 30% ambient light
+// 	mini.amb.color = (t_vec3){255, 255, 255};  // White ambient light
 
-	// ADD LIGHT SOURCE (THIS WAS MISSING!)
-	mini.light.light_point = (t_vec3){5, 5, 0};
-	mini.light.lbr = 1.0;
-	mini.light.color = (t_vec3){255, 255, 255};
+// 	// ADD LIGHT SOURCE (THIS WAS MISSING!)
+// 	mini.light.light_point = (t_vec3){5, 5, 0};
+// 	mini.light.lbr = 1.0;
+// 	mini.light.color = (t_vec3){255, 255, 255};
 
-	// Render the scene
-	render_scene(&mini);
+// 	// Render the scene
+// 	render_scene(&mini);
 
-	// Display and wait
-	mlx_put_image_to_window(mini.mlx_utils.mlx, mini.mlx_utils.win,
-		mini.mlx_utils.img, 0, 0);
-	mlx_loop(mini.mlx_utils.mlx);
+// 	// Display and wait
+// 	mlx_put_image_to_window(mini.mlx_utils.mlx, mini.mlx_utils.win,
+// 		mini.mlx_utils.img, 0, 0);
+// 	mlx_loop(mini.mlx_utils.mlx);
 
-	// Cleanup
-	free(mini.sph);
-	free(mini.plane);
-	return (0);
-}
+// 	// Cleanup
+// 	free(mini.sph);
+// 	free(mini.plane);
+// 	return (0);
+// }
+
+// av = 
+
+
+// int f1(char **s)
+// {
+// 	while(**s)
+// 	{
+// 		(*s)++;
+// 	}
+// }
+// int main (int ac, char *av[])
+
+// {
+// 	printf("%p\n", *av);
+// 	f1(++av);
+// 	printf("%p\n", **av);
+// }
